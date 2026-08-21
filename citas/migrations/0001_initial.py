59042dcf557b359789086f493059415b077eb134
alewoodz <undimoraurio@gmail.com>
2026-08-20 22:18:38 -0400

    clase git 20-08-2026

---
+++ citas/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Cliente',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nombre', models.CharField(max_length=100)),
+                ('telefono', models.CharField(blank=True, max_length=20)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Servicio',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nombre', models.CharField(max_length=80)),
+                ('precio', models.IntegerField()),
+                ('duracion_min', models.IntegerField(default=30)),
+            ],
+        ),
+    ]
